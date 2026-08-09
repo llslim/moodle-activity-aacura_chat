2f0ca2d9ff5b774e5b1dce0a22028f9032b998d7
Kevin Williams <llslim@gmail.com>
2026-08-08 22:40:57 -0400

    Fix CSS selectors prefix to match renamed mod_aacurachat activity name

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026052505;
+$plugin->version = 2026052506;
 $plugin->release = "1.1.0";
 $plugin->requires = 2026042000;
 $plugin->maturity = MATURITY_STABLE;
